db52e15949dec51c67f4bee66e6855c0bd3b74b0
Jean-Philippe Gariépy <Jean-Philippe.Gariepy@nuecho.com>
2013-11-11 12:03:09 -0500

    VoicemailDialogue.java: changed static import for Interaction.Builder factory method.

--- src/main/java/com/nuecho/rivr/samples/voicemail/dialogue/VoicemailDialogue.java
+++ src/main/java/com/nuecho/rivr/samples/voicemail/dialogue/VoicemailDialogue.java
@@ -4,7 +4,7 @@
 
 package com.nuecho.rivr.samples.voicemail.dialogue;
 
-import static com.nuecho.rivr.voicexml.turn.output.Interaction.Builder.*;
+import static com.nuecho.rivr.voicexml.turn.OutputTurns.*;
 import static java.lang.String.*;
 
 import java.util.regex.*;
